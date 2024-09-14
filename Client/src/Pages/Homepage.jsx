904dea9eaf8e2d5d3e73507dd05aac5db9ff454b
pawan85074 <pawanraj85074@gmail.com>
2024-09-14 13:29:26 +0530

    add navbar

---
+++ Client/src/Pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Homepage = () => {
+  return (
+    <>
+        <Navbar/>
+    </>
+  )
+}
+
+export default Homepage
